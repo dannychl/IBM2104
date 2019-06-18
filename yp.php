0702aa0ffa889406c8ac718d4fc16802076671f8
leong34 <51914391+leong34@users.noreply.github.com>
2019-06-18 10:09:12 +0800

    Update yp.php

--- yp.php
+++ yp.php
@@ -15,37 +15,99 @@
 			<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
 
 			<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+			
+			<style>
+				.rating {
+				  unicode-bidi: bidi-override;
+				  direction: rtl;
+				}
+				.rating > span {
+				  display: inline-block;
+				  position: relative;
+				  width: 1.1em;
+				}
+				.rating > span:hover:before,
+				.rating > span:hover ~ span:before {
+				   content: "\2605";
+				   position: absolute;
+				}
+			</style>
+
 			</head>
 			
 			<body>
-				<div class="container">
+
+
+				<div class="container" style="margin-top: 100px; margin-left: 100px;">
 					<div class="row">
-						<div class="col-md-9">
+
+						<div class="col-md-4 mt-5" style="width: 100%">
+							<div class="panel panel-primary">
+								<div class="panel-heading">
+								<center><h4>Advanced Search</h4></center>
+								</div>
+
+								<div class="panel-body ml-5 mt-3">
+								<h4>State: 
+									<select name="state">
+									<option value="0">State</option>
+									</select> 
+								</h4>
+								</div>
+
+								<div class="panel-footer">
+								</div>
+							</div>
+						</div>
+
+
+
+						<div class="col-md-8" style="width: 60%">
 						  <table class="table table-hover" style="margin-left: 200px;">
 								<tbody>
 								  <tr style="height: 150px;">
 									<td><img src="inti-logo.png" alt="Inti Logo" style="height: 100px; margin-top: 15px; margin-left: 15px;"></td>	
-									<td><br><br>INTI International College Penang<br><a href="new 2.php">See More...</a></td>
+									<td><br><br>INTI International College Penang
+									<br><span class="rating" style="margin-right: 250px;">
+										<span>☆</span><span>☆</span><span>☆</span><span>☆</span><span>☆</span>
+										</span>	
+									<br><a href="new 2.php">See More...</a></td>
 								  </tr>
 
 								  <tr style="height: 150px;">
 								  	<td><img src="segi.png" alt="SEGI Logo" style="height: 100px; margin-top: 18px; margin-left: 15px;"></td>
-								  	<td><br><br>SEGI University & College<br><a href="https://www.google.com">See More...</a></td>
+								  	<td><br><br>SEGI University & College
+								  	<br><span class="rating" style="margin-right: 250px;">
+										<span>☆</span><span>☆</span><span>☆</span><span>☆</span><span>☆</span>
+										</span>	
+								  	<br><a href="https://www.google.com">See More...</a></td>
 								  </tr>
 
 								   <tr style="height: 150px;">
 								  	<td><img src="tarc.png" alt="TARC Logo" style="height: 100px; margin-top: 18px; margin-left: 15px;"></td>
-								  	<td><br><br>Tunku Abdul Rahman University College<br><a href="https://www.google.com">See More...</a></td>
+								  	<td><br><br>Tunku Abdul Rahman University College
+								  	<br><span class="rating" style="margin-right: 250px;">
+										<span>☆</span><span>☆</span><span>☆</span><span>☆</span><span>☆</span>
+										</span>	
+								  	<br><a href="https://www.google.com">See More...</a></td>
 								  </tr>
 
 								   <tr style="height: 150px;">
 								  	<td><img src="sunway.png" alt="Sunway Logo" style="height: 100px; margin-top: 18px; margin-left: 15px;"></td>
-								  	<td><br><br>Sunway College<br><a href="https://www.google.com">See More...</a></td>
+								  	<td><br><br>Sunway College
+								  	<br><span class="rating" style="margin-right: 250px;">
+										<span>☆</span><span>☆</span><span>☆</span><span>☆</span><span>☆</span>
+										</span>	
+								  	<br><a href="https://www.google.com">See More...</a></td>
 								  </tr>
 
 								  <tr style="height: 150px;">
 								  	<td><img src="disted.png" alt="Disted Logo" style="height: 100px; margin-top: 18px; margin-left: 15px;"></td>
-								  	<td><br><br>Disted College<br><a href="https://www.google.com">See More...</a></td>
+								  	<td><br><br>Disted College
+								  	<br><span class="rating" style="margin-right: 250px;">
+										<span>☆</span><span>☆</span><span>☆</span><span>☆</span><span>☆</span>
+										</span>	
+								  	<br><a href="https://www.google.com">See More...</a></td>
 								  </tr>
 								  
 								</tbody>
