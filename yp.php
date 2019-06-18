c6344cc022c76bb20b8dc5111403d46f2f8d35c4
leong34 <51914391+leong34@users.noreply.github.com>
2019-06-18 09:05:21 +0800

    Add files via upload

---
+++ yp.php
@@ -0,0 +1,60 @@
+<?php
+	echo   '<html>
+			<head>
+			<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+			<!-- Latest compiled and minified CSS -->
+			<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+
+			<!-- jQuery library -->
+			<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
+
+			<!-- Popper JS -->
+			<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+
+			<!-- Latest compiled JavaScript -->
+			<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+
+			<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+			</head>
+			
+			<body>
+				<div class="container">
+					<div class="row">
+						<div class="col-md-9">
+						  <table class="table table-hover" style="margin-left: 200px;">
+								<tbody>
+								  <tr style="height: 150px;">
+									<td><img src="inti-logo.png" alt="Inti Logo" style="height: 100px; margin-top: 15px; margin-left: 15px;"></td>	
+									<td><br><br>INTI International College Penang<br><a href="new 2.php">See More...</a></td>
+								  </tr>
+
+								  <tr style="height: 150px;">
+								  	<td><img src="segi.png" alt="SEGI Logo" style="height: 100px; margin-top: 18px; margin-left: 15px;"></td>
+								  	<td><br><br>SEGI University & College<br><a href="https://www.google.com">See More...</a></td>
+								  </tr>
+
+								   <tr style="height: 150px;">
+								  	<td><img src="tarc.png" alt="TARC Logo" style="height: 100px; margin-top: 18px; margin-left: 15px;"></td>
+								  	<td><br><br>Tunku Abdul Rahman University College<br><a href="https://www.google.com">See More...</a></td>
+								  </tr>
+
+								   <tr style="height: 150px;">
+								  	<td><img src="sunway.png" alt="Sunway Logo" style="height: 100px; margin-top: 18px; margin-left: 15px;"></td>
+								  	<td><br><br>Sunway College<br><a href="https://www.google.com">See More...</a></td>
+								  </tr>
+
+								  <tr style="height: 150px;">
+								  	<td><img src="disted.png" alt="Disted Logo" style="height: 100px; margin-top: 18px; margin-left: 15px;"></td>
+								  	<td><br><br>Disted College<br><a href="https://www.google.com">See More...</a></td>
+								  </tr>
+								  
+								</tbody>
+						  </table>
+						  </table>
+						</div>
+					</div>
+				</div>
+			</body>
+			</html>'
+
+?>
