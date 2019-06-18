fdb39772ca8fd6e995c67c682d8b581906a4aba2
leong34 <51914391+leong34@users.noreply.github.com>
2019-06-18 17:45:01 +0800

    Update yp.php

--- yp.php
+++ yp.php
@@ -16,6 +16,9 @@
 
 			<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
 			
+			<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+			<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
 			<style>
 				.rating {
 				  unicode-bidi: bidi-override;
@@ -41,21 +44,25 @@
 				<div class="container" style="margin-top: 100px; margin-left: 100px;">
 					<div class="row">
 
-						<div class="col-md-4 mt-5" style="width: 100%">
-							<div class="panel panel-primary">
-								<div class="panel-heading">
+						<div class="col-md-4" style="width: 100%; margin-top: 150px">
+							<div class="panel panel-primary" style="border-radius: 25px;">
+								<div class="panel-heading" style="border-radius: 25px 25px 0px 0px;">
 								<center><h4>Advanced Search</h4></center>
 								</div>
 
 								<div class="panel-body ml-5 mt-3">
 								<h4>State: 
-									<select name="state">
-									<option value="0">State</option>
+									<select name="state" style="margin-left: 20px; border-radius: 10px;">
+									<option value="0">------State------</option>
+									<option value="1">Kuala Lumpur</option>
+									<option value="2">Penang</option>
+									<option value="3">Perak</option>
 									</select> 
 								</h4>
 								</div>
 
-								<div class="panel-footer">
+								<div class="panel-footer" style="border-radius: 0px 0px 25px 25px;">
+									<center><button type="button" class="btn btn-success" style="height: 40px; width: 100px; border-radius: 18px;">Search <span class="fa fa-search"</span></button></center>
 								</div>
 							</div>
 						</div>
@@ -67,46 +74,38 @@
 								<tbody>
 								  <tr style="height: 150px;">
 									<td><img src="inti-logo.png" alt="Inti Logo" style="height: 100px; margin-top: 15px; margin-left: 15px;"></td>	
-									<td><br><br>INTI International College Penang
-									<br><span class="rating" style="margin-right: 250px;">
-										<span>☆</span><span>☆</span><span>☆</span><span>☆</span><span>☆</span>
-										</span>	
+									<td><br>INTI International College Penang
+									<br><span style="color: B7B7B7">Rating: 4.5</span>
+									<br><span class="material-icons" style="color: B7B7B7; font-size: 20px;">place</span><span style="color: B7B7B7">Kuala Lumpur</span>
 									<br><a href="new 2.php">See More...</a></td>
 								  </tr>
 
 								  <tr style="height: 150px;">
 								  	<td><img src="segi.png" alt="SEGI Logo" style="height: 100px; margin-top: 18px; margin-left: 15px;"></td>
-								  	<td><br><br>SEGI University & College
-								  	<br><span class="rating" style="margin-right: 250px;">
-										<span>☆</span><span>☆</span><span>☆</span><span>☆</span><span>☆</span>
-										</span>	
+								  	<td><br>SEGI University & College
+								  	<br><span style="color: B7B7B7">Rating: 4.5</span>
+								  	<br><span class="material-icons" style="color: B7B7B7; font-size: 20px;">place</span><span style="color: B7B7B7">Penang</span>
 								  	<br><a href="https://www.google.com">See More...</a></td>
 								  </tr>
 
 								   <tr style="height: 150px;">
 								  	<td><img src="tarc.png" alt="TARC Logo" style="height: 100px; margin-top: 18px; margin-left: 15px;"></td>
-								  	<td><br><br>Tunku Abdul Rahman University College
-								  	<br><span class="rating" style="margin-right: 250px;">
-										<span>☆</span><span>☆</span><span>☆</span><span>☆</span><span>☆</span>
-										</span>	
+								  	<td><br>Tunku Abdul Rahman University College
+								  	<br><span style="color: B7B7B7">Rating: 4.5</span><br><span class="material-icons" style="color: B7B7B7; font-size: 20px;">place</span><span style="color: B7B7B7">Penang</span>
 								  	<br><a href="https://www.google.com">See More...</a></td>
 								  </tr>
 
 								   <tr style="height: 150px;">
 								  	<td><img src="sunway.png" alt="Sunway Logo" style="height: 100px; margin-top: 18px; margin-left: 15px;"></td>
-								  	<td><br><br>Sunway College
-								  	<br><span class="rating" style="margin-right: 250px;">
-										<span>☆</span><span>☆</span><span>☆</span><span>☆</span><span>☆</span>
-										</span>	
+								  	<td><br>Sunway College
+								  	<br><span style="color: B7B7B7">Rating: 4.5</span><br><span class="material-icons" style="color: B7B7B7; font-size: 20px;">place</span><span style="color: B7B7B7">Penang</span>
 								  	<br><a href="https://www.google.com">See More...</a></td>
 								  </tr>
 
 								  <tr style="height: 150px;">
 								  	<td><img src="disted.png" alt="Disted Logo" style="height: 100px; margin-top: 18px; margin-left: 15px;"></td>
-								  	<td><br><br>Disted College
-								  	<br><span class="rating" style="margin-right: 250px;">
-										<span>☆</span><span>☆</span><span>☆</span><span>☆</span><span>☆</span>
-										</span>	
+								  	<td><br>Disted College
+								  	<br><span style="color: B7B7B7">Rating: 4.5</span><br><span class="material-icons" style="color: B7B7B7; font-size: 20px;">place</span><span style="color: B7B7B7">Penang</span>
 								  	<br><a href="https://www.google.com">See More...</a></td>
 								  </tr>
 								  
