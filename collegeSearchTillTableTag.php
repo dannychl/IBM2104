694e78e0f8bf0b677b811acfffb1637a99b39b0c
leong34 <51914391+leong34@users.noreply.github.com>
2019-06-24 20:28:24 +0800

    Update collegeSearchTillTableTag.php

--- collegeSearchTillTableTag.php
+++ collegeSearchTillTableTag.php
@@ -19,6 +19,7 @@ include('header1.html');
 				
 				<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
 				<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+				
 				<script>
 				jQuery(document).ready(function($) {
 				    $(".clickable-row").click(function() {
